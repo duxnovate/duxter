2034a73c203ad7c380bb465c287e7dcaa8f1dd4b
farhank15 <afarhank22@gmail.com>
2024-10-07 02:18:45 +0700

    remove all

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyInstance } from "fastify";
-import { helloworld } from "@routes/index.js";
-
-const registerRoutes = (fastify: FastifyInstance) => {
-  fastify.register(helloworld);
-};
-
-export default registerRoutes;
--- src/routes/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as helloworld } from "./helloworld";
